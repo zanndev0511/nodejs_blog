776a5efd59671086c1576f257b1d5c468a2cb2a8
zanndev0511 <giangtruong051103@gmail.com>
2023-05-30 03:17:41 +0700

    Install prettier, lint-staged, husky

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,11 +1,11 @@
 class SiteController {
-  // [GET] /
-  index(req, res) {
-    res.render("home");
-  }
-  // [GET] /search
-  search(req, res) {
-    res.render("search");
-  }
+    // [GET] /
+    index(req, res) {
+        res.render('home');
+    }
+    // [GET] /search
+    search(req, res) {
+        res.render('search');
+    }
 }
 module.exports = new SiteController();
--- src/routes/news.js
+++ src/routes/news.js
@@ -1,9 +1,9 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const newsController = require("../app/controllers/NewsController.js");
+const newsController = require('../app/controllers/NewsController.js');
 
-router.use("/:slug", newsController.show);
-router.use("/", newsController.index);
+router.use('/:slug', newsController.show);
+router.use('/', newsController.index);
 
 module.exports = router;
