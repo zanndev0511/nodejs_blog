6a660fdca7f509f7ab8aa57d356323473cc79d3e
zanndev0511 <giangtruong051103@gmail.com>
2023-06-22 23:49:24 +0700

    update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const path = require("path");
 const morgan = require("morgan");
 // const handlebars = require("express-handlebars");
 // const express = require("express");
-const { engine } = require("express-handlebars");
+const engine = require('express-handlebars');
 const app = express();
 const port = 3000;
 
@@ -24,6 +24,19 @@ app.use(
 app.use(express.json());
 
 app.use(methodOverride("_method"));
+
+app.get('/middleware', 
+function (req, res, next){
+  if(['vethuong', 'vevip'].includes(req.query.ve)){
+    return next()
+  }
+  res.status(403).json({message: "Access denied"})
+},
+function(req, res, next){
+  res.json({
+    message: 'successfully'
+  })
+})
 
 // HTTP logger
 // app.use(morgan("combined"));
